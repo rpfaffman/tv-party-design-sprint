9da121125beb0f2c38e8afbd3380cb8c10c1399c
Roy Pfaffman <roy.pfaffman@vevo.com>
2016-07-28 22:52:52 -0700

    Removes example module; queue back links to search

--- routes/queue/components/header.js
+++ routes/queue/components/header.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import styles from './header.scss';
+import { Link } from 'react-router';
 
 export default class Header extends Component {
   render() {
     return (
       <div className={ styles.default }>
-        <img src='images/back-arrow-icon.svg' className={ styles['back-arrow'] } />
+        
+        <Link to="/">
+          <img src='images/back-arrow-icon.svg' className={ styles['back-arrow'] } />
+        </Link>
         <div className={ styles['header'] }>Playlist Queue</div>
       </div>
     );
