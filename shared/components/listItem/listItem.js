daed8d157c8d5966ba6a995e834b94f0e8e1aada
Mitchell Wilcox <mitchell.wilcox@vevo.com>
2016-07-28 19:41:21 -0700

    styling

--- shared/components/listItem/listItem.js
+++ shared/components/listItem/listItem.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react'
 import styles from './listItem.scss';
 
-const thumbParamsWide = '?crop=auto&scale=both&width=117&height=60'
+const thumbParamsWide = '?crop=auto&scale=both&width=423&height=220'
 
 // import logo from 'images/add.png'
 
@@ -12,10 +12,10 @@ export default class ListItem extends Component {
     const {item} = this.props
     return (
       <div className={styles.default}>
-        <img src={`${item.thumbnailUrl}${thumbParamsWide}`} />
+        <img className={styles.thumbnail} src={`${item.thumbnailUrl}${thumbParamsWide}`} />
         <div className={styles.details}>
           <div className={styles.artist}>{item.primaryArtists[0].name}</div>
-          <div className={styles.title}> {item.title}</div>
+          <div className={styles.title}>{item.title}</div>
         </div>
         <div className={styles.btnAdd}>
           <a href="javascript:void(0)" onClick={() => {this.props.onClick(item)}}>Add</a>
